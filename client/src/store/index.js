17deac6a2779f3071d8bcd61d771bda6cf6a8127
Sebastian Tan <sebthtan@yahoo.com>
2021-06-11 14:06:16 -0500

    removed commented-out preloadedState in redux store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -28,7 +28,7 @@ const rootReducer = (state, action) => {
 };
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+const store = createStore(rootReducer, composeEnhancers(
   applyMiddleware(thunkMiddleware, loggerMiddleware)
 ))
 

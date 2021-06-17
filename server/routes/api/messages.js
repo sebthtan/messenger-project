6e7eb7b41cf14b24dc2a2b385c6f84bee1e21d06
Sebastian Tan <sebthtan@yahoo.com>
2021-06-17 17:51:26 -0500

    updated put route

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -58,7 +58,7 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-router.put('/', async (req, res, next) => {
+router.put('/read', async (req, res, next) => {
   try {
     if (!req.user) {
       return res.sendStatus(401);
